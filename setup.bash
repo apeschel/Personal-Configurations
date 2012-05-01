7701521cc1bd3eb9f376374549378ac19f470cdc
Aaron Peschel <apeschel@zendesk.com>
2012-05-01 23:02:01 +0000

    Ignore vim errors on first run.

--- setup.bash
+++ setup.bash
@@ -22,4 +22,4 @@ if [[ ! -e $vundle_dir ]]; then
     git clone https://github.com/gmarik/vundle.git $vundle_dir
 fi
 
-vim -c "BundleInstall" -c "only"
+vim -c "BundleInstall" -c "only" 2>/dev/null
