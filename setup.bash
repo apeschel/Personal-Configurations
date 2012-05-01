a04ba46dfdf5e50d021d716c1dcb57d51dec0b92
Aaron Peschel <apeschel@zendesk.com>
2012-05-01 19:03:44 +0000

    More fully fleshed out setup script.

--- setup.bash
+++ setup.bash
@@ -5,7 +5,21 @@ files=( .vim .vimrc .bashrc .zshrc .tmux.conf )
 for file in ${files[@]}; do
     dest="${HOME}/${file}"
 
-    if [[ ! -f $dest ]]; then
-        ln -s "${HOME}/Personal-Configurations/${file}" "$dest"
+    if [[ ! -e $dest ]]; then
+        echo ln -s "${HOME}/Personal-Configurations/${file}" "$dest"
     fi
 done
+
+vim_dir="${HOME}/.vim"
+for dir in ${vim_dir}/{tmp,backup}; do
+    if [[ ! -e $dir ]]; then
+        mkdir $dir
+    fi
+done
+
+vundle_dir="${vim_dir}/bundle/vundle"
+if [[ ! -e $vundle_dir ]]; then
+    git clone https://github.com/gmarik/vundle.git $vundle_dir
+fi
+
+vim -c "BundleInstall" -c "only"
