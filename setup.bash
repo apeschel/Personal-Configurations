939179db7e2d79385a6338bc532197ff8cdd4b1e
Aaron Peschel <aaron.peschel@gmail.com>
2021-01-05 18:07:40 -0800

    Update Setup Script

--- setup.bash
+++ setup.bash
@@ -14,24 +14,30 @@ files+=(.vim .vimrc)
 # Configs for readline like libraries.
 files+=(.inputrc .haskeline .editrc)
 
-for file in ${files[@]}; do
+# Nethack
+files+=(.nethackrc)
+
+# GPG
+files+=(.gnupg/gpg.conf .gnupg/gpg-agent.conf)
+
+for file in "${files[@]}"; do
     dest="${HOME}/${file}"
 
-    if [[ ! -e $dest ]]; then
+    if [[ ! -e "$dest" ]]; then
         ln -s "${HOME}/Personal-Configurations/${file}" "$dest"
     fi
 done
 
 vim_dir="${HOME}/.vim"
 for dir in ${vim_dir}/{tmp,backup}; do
-    if [[ ! -e $dir ]]; then
-        mkdir $dir
+    if [[ ! -e "$dir" ]]; then
+        mkdir "$dir"
     fi
 done
 
 vundle_dir="${vim_dir}/bundle/vundle"
-if [[ ! -e $vundle_dir ]]; then
-    git clone https://github.com/gmarik/vundle.git $vundle_dir
+if [[ ! -e "$vundle_dir" ]]; then
+    git clone https://github.com/gmarik/vundle.git "$vundle_dir"
 fi
 
 vim -c "BundleInstall" -c "only" 2>/dev/null
